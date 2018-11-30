97692ce0667313b5bf85b8d23341b01cb3249b73
trinh04042000 <letrinh04042000@gmail.com>
2018-11-30 11:18:50 +0700

    add new file

---
+++ resources/lang/vi/language.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'blog_application' => 'Ứng dụng Blog',
+    'home' => 'Trang chủ',
+    'list_post' => 'Danh Sách Bài Viết',
+    'create_post' => 'Tạo Bài Viết',
+    'save' => 'Lưu',
+    'post_not_exist' => 'Post not exist',
+    'title' => 'Title',
+    'content' => 'Content',
+];
